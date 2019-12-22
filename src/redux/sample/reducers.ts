40504f59ce48a8b2f3a67bd3f493ca03f9d32303
n-inokawa <inkwmwmw@gmail.com>
2019-12-22 16:09:16 +0900

    Add data payload to sampleFetchSucceed action

--- src/redux/sample/reducers.ts
+++ src/redux/sample/reducers.ts
@@ -1,15 +1,22 @@
 import { SAMPLE_FETCH_REQUEST, SAMPLE_FETCH_SUCCEED, SAMPLE_FETCH_FAILED } from './types';
 import { Actions } from './actions';
 
-type State = {};
+type State = {
+  data: string;
+};
 
-const initialState: State = {};
+const initialState: State = {
+  data: ''
+};
 
 const reducer = (state: State = initialState, action: Actions): State => {
   switch (action.type) {
     case SAMPLE_FETCH_REQUEST:
+      return { ...state };
     case SAMPLE_FETCH_SUCCEED:
+      return { ...state, data: action.payload.data };
     case SAMPLE_FETCH_FAILED:
+      return { ...state };
     default:
       return { ...state };
   }
