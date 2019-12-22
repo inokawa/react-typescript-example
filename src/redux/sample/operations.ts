158b7bfc957bfdc49794b81893bdeea5bdcc276a
n-inokawa <inkwmwmw@gmail.com>
2019-12-22 15:08:34 +0900

    Create fetchSample operation

---
+++ src/redux/sample/operations.ts
@@ -0,0 +1,7 @@
+import { Operation } from '../types';
+import { sampleFetchRequest, sampleFetchSucceed, } from './actions';
+
+export const fetchSample = (): Operation => async dispatch => {
+    dispatch(sampleFetchRequest());
+    dispatch(sampleFetchSucceed());
+};
