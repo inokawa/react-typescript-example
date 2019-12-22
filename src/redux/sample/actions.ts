dfac21a435d1b1a250eedb83787fb3d35a93c54c
n-inokawa <inkwmwmw@gmail.com>
2019-12-22 14:56:42 +0900

    Apply Actions type to sample reducer

--- src/redux/sample/actions.ts
+++ src/redux/sample/actions.ts
@@ -4,3 +4,5 @@ import { Action } from '../types';
 export const sampleFetchRequest = (): Action => ({ type: SAMPLE_FETCH_REQUEST, payload: {} });
 export const sampleFetchSucceed = (): Action => ({ type: SAMPLE_FETCH_SUCCEED, payload: {} });
 export const sampleFetchFailed = (): Action => ({ type: SAMPLE_FETCH_FAILED, payload: {} });
+
+export type Actions = ReturnType<typeof sampleFetchRequest | typeof sampleFetchSucceed | typeof sampleFetchFailed>;
