e48a408c9d71f8fe7f371e53eb2da9f395a5ca47
n-inokawa <48897392+n-inokawa@users.noreply.github.com>
2019-12-22 14:56:42 +0900

    Apply Actions type to sample reducer

--- src/redux/sample/reducers.ts
+++ src/redux/sample/reducers.ts
@@ -1,11 +1,15 @@
-import { AnyAction } from 'redux';
+import { SAMPLE_FETCH_REQUEST, SAMPLE_FETCH_SUCCEED, SAMPLE_FETCH_FAILED } from './types';
+import { Actions } from './actions';
 
 type State = {};
 
 const initialState: State = {};
 
-const reducer = (state: State = initialState, action: AnyAction): State => {
+const reducer = (state: State = initialState, action: Actions): State => {
   switch (action.type) {
+    case SAMPLE_FETCH_REQUEST:
+    case SAMPLE_FETCH_SUCCEED:
+    case SAMPLE_FETCH_FAILED:
     default:
       return { ...state };
   }
