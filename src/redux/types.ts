93228bccc53946c6403b66ddce96ef6c6a411431
n-inokawa <inkwmwmw@gmail.com>
2019-12-22 16:04:28 +0900

    Remove Action type

--- src/redux/types.ts
+++ src/redux/types.ts
@@ -2,6 +2,4 @@ import { AnyAction } from 'redux';
 import { ThunkAction } from 'redux-thunk';
 import { AppState } from './rootReducer';
 
-export type Action = AnyAction & { payload: any };
-
-export type Operation = ThunkAction<void, AppState, null, Action>;
+export type Operation = ThunkAction<void, AppState, null, AnyAction>;
