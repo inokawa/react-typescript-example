14a731255f75b6238363d53874e4bed06061b9ae
n-inokawa <48897392+n-inokawa@users.noreply.github.com>
2019-12-22 15:06:34 +0900

    Create Operation type

--- src/redux/types.ts
+++ src/redux/types.ts
@@ -1,3 +1,7 @@
 import { AnyAction } from 'redux';
+import { ThunkAction } from 'redux-thunk';
+import { AppState } from './rootReducer';
 
 export type Action = AnyAction & { payload: any };
+
+export type Operation = ThunkAction<void, AppState, null, Action>;
