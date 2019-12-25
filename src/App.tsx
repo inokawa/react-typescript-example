7396a98af8962a065c5d926349f25c4ab9bbc9bc
n-inokawa <48897392+n-inokawa@users.noreply.github.com>
2019-12-25 23:28:47 +0900

    Separate Main page from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,33 +3,15 @@ import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import rootReducer from './redux/rootReducer';
-import logo from './logo.svg';
 import './App.css';
-import Button from './components/Button';
+import Main from './pages/Main';
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
 const App: React.FC = () => {
-  const onClick = React.useCallback(() => alert('clicked'), []);
   return (
     <Provider store={store}>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-        </a>
-          <Button onClick={onClick} title="alert" />
-        </header>
-      </div>
+      <Main />
     </Provider>
   );
 }
