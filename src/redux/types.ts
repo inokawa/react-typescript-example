6bf67b724111b304d5c2a08fbcb2a655612d15b1
n-inokawa <48897392+n-inokawa@users.noreply.github.com>
2019-12-25 23:03:27 +0900

    Move AppState type into src/redux/types.ts

--- src/redux/types.ts
+++ src/redux/types.ts
@@ -1,5 +1,7 @@
 import { AnyAction } from 'redux';
 import { ThunkAction } from 'redux-thunk';
-import { AppState } from './rootReducer';
+import reducer from './rootReducer';
+
+export type AppState = ReturnType<typeof reducer>;
 
 export type Operation = ThunkAction<void, AppState, null, AnyAction>;
