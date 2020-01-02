c33b29f4cc8da5965abd59afd81ddebe25164cf2
n-inokawa <48897392+n-inokawa@users.noreply.github.com>
2020-01-02 15:21:00 +0900

    Create AppDispatch type

--- src/redux/types.ts
+++ src/redux/types.ts
@@ -1,7 +1,10 @@
 import { AnyAction } from 'redux';
 import { ThunkAction } from 'redux-thunk';
 import reducer from './rootReducer';
+import configureStore from './configureStore';
 
 export type AppState = ReturnType<typeof reducer>;
+
+export type AppDispatch = ReturnType<typeof configureStore>['dispatch'];
 
 export type Operation = ThunkAction<void, AppState, null, AnyAction>;
