0783cf188cb38ccc8db14294644bf8eaf3a8fdca
n-inokawa <48897392+n-inokawa@users.noreply.github.com>
2020-04-29 17:38:21 +0900

    Add .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {
+    semi: ["error", "always"],
+    indent: ["error", 2],
+  },
+};
