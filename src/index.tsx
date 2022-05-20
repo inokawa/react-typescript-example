381f6c18eed8ea325e82e9e720701833b9ca1aaa
inokawa <48897392+inokawa@users.noreply.github.com>
2022-05-20 12:51:21 +0900

    Use strict mode

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,10 @@
+import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
