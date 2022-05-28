bf39715ade84511e6ecf826a25d76159eafcb470
inokawa <48897392+inokawa@users.noreply.github.com>
2022-05-28 17:58:09 +0900

    Run jest in esm mode

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,16 @@
 module.exports = {
   testEnvironment: "jsdom",
+  // for Jest ESM support
+  transform: {
+    "\\.[jt]sx?$": "ts-jest",
+  },
+  extensionsToTreatAsEsm: [".ts", ".tsx"],
+  globals: {
+    "ts-jest": {
+      useESM: true,
+    },
+  },
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
 };
